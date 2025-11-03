ce9897f87077eca248928b72d487a30b6c938e66
Postali Web Development <181128403+Postali-Webdev@users.noreply.github.com>
2025-11-03 17:22:00 -0500

    moving clickcease to footer

--- crest-child/footer.php
+++ crest-child/footer.php
@@ -334,5 +334,17 @@ $remove_footer = get_field('remove_footer');
     })(window, document, "clarity", "script", "5ksm4db84a");
 </script>
 
+<!--  ClickCease.com tracking-->
+<script type='text/javascript'>var script = document.createElement('script');
+script.async = true; script.type = 'text/javascript';
+var target = 'https://www.clickcease.com/monitor/stat.js';
+script.src = target;var elem = document.head;elem.appendChild(script);
+</script>
+<noscript>
+<a href='https://www.clickcease.com' rel='nofollow'><img src='https://monitor.clickcease.com' alt='ClickCease'/></a>
+</noscript>
+<!--  ClickCease.com tracking-->
+
+
 </body>
 </html>
