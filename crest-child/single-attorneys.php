5cb21c1197466ec299ab845f4e6d8e978c7a08fd
Eric Schumacher <webdev@postali.com>
2026-06-05 15:33:38 -0400

    fixes to attorney bio template

--- crest-child/single-attorneys.php
+++ crest-child/single-attorneys.php
@@ -62,6 +62,113 @@ get_header();
     </div>
 </section>
 
+<?php
+if (get_field('staff_type') == 'attorney' ) { ?>
+
+
+<section class="testimonials">
+    <div class="container">
+        <div class="columns">
+            <div id="written-reviews" class="testimonials-block ">
+            <?php
+                // get the current attorney slug (from the single attorney post)
+                $current_slug = get_post_field( 'post_name', get_the_ID() );
+
+                $written_args = [
+                    'post_type' => 'reviews',
+                    'post_status' => 'publish',
+                    'posts_per_page' => 3,
+                    'order' => 'DESC',
+                    'meta_query' => [
+                        [
+                            'key' => 'attorney',
+                            'value' => get_the_ID(),
+                            'compare' => '='
+                        ]
+                    ]
+                ];
+
+                $written_query = new WP_Query( $written_args );
+
+                $reviews = $written_query->posts;
+
+                if ( count($reviews) < 3 ) {
+                    $remaining = 3 - count($reviews);
+                    $empty_args = [
+                        'post_type' => 'reviews',
+                        'post_status' => 'publish',
+                        'posts_per_page' => $remaining,
+                        'order' => 'DESC',
+                        'meta_query' => [
+                            'relation' => 'OR',
+                            [
+                                'key' => 'attorney',
+                                'compare' => 'NOT EXISTS'
+                            ],
+                            [
+                                'key' => 'attorney',
+                                'value' => '',
+                                'compare' => '='
+                            ]
+                        ],
+                        'post__not_in' => wp_list_pluck($reviews, 'ID')
+                    ];
+                    $empty_query = new WP_Query( $empty_args );
+                    $reviews = array_merge($reviews, $empty_query->posts);
+                }
+
+                // Remove duplicate posts just in case
+                $reviews = array_unique($reviews, SORT_REGULAR);
+
+                // Setup loop for $reviews
+                global $post;
+                $original_post = $post;
+                foreach ($reviews as $review_post) {
+                    $post = $review_post;
+                    setup_postdata($post);
+                    $review_source = get_field('review_source');
+                    switch( $review_source ) {
+                        case 'google':
+                            $review_badge = "/wp-content/uploads/2025/04/google-review-logo.png";
+                            break;
+                        case 'avvo':
+                            $review_badge = "/wp-content/uploads/2025/04/avvo-logo.png";
+                            break;
+                        default:
+                            $review_badge = "/wp-content/uploads/2025/04/google-review-logo.png";
+                            break;
+                    }
+            ?>
+
+                <div class="testimonial">
+                    <div class="rating-row">
+                        <p class="average"><?php the_field('reviews_average_rating', 'options'); ?></p>
+                        <div class="stars"></div>
+                    </div>
+                    <div class="copy"><p><?php echo substr(get_the_content($post->ID), 0, 300); ?>
+                    <?php if( strlen(get_the_content($post->ID)) > 300) : ?>
+                        [...]
+                    <?php endif; ?>
+                    </p></div>
+                    <div class="author-row">
+                        <p class="author"><?php echo get_the_title($post->ID); ?></p>
+                        <a href="<?php echo $gbp_url; ?>" target="_blank">
+                            <img src="<?php echo esc_url($review_badge); ?>" alt="<?php echo $review_source; ?> review of Breeden Law Firm" />
+                        </a>
+                    </div>
+                </div>
+            <?php }
+                $post = $original_post;
+                wp_reset_postdata();
+            ?>
+            
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php } ?>
+
 <?php get_template_part('block', 'global-prefooter-cta'); ?>
 
 
